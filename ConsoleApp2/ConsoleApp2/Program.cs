3f49aaa64ec3a244624d36dcb0a96c23dac2594f
Julia <119501186+JuliaMalkina13101996@users.noreply.github.com>
2022-12-16 15:04:51 +0400

    Домашка Семинар 2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿//Console.WriteLine("input number");
+//int number = int.Parse(Console.ReadLine());
+//int number1 = number % 100 / 10;
+//Console.WriteLine($"вторая цифра в числе: {number1}");
+
+//Console.WriteLine("введите число: ");
+//int number =int.Parse(Console.ReadLine());
+//int a = 10;
+//if (number > 99)
+//{
+//    while (number > 1000)
+//    {
+//        number = number / a;
+//    }
+//    number = number % 10;
+
+//    Console.Write(number);
+//}
+//else
+//{
+//    Console.WriteLine("нет");
+//}
+
+//int day = int.Parse(Console.ReadLine());
+//if (day ==6  || day ==7)
+//{
+//    Console.Write("выходной");
+//}
+//else
+//{
+//    Console.Write("будний день");
+//}
